csyonghe/Spire

commit 1eee18256190996eddf8c1613ef3fffae0caa372

Remove `StructSymbol`
We just use `StructSyntaxNode` for everything front-end related, and then have the code generator maintain a map from `StructSyntaxNode*` to `ILStructType` that it fills in as needed.

The main complication in this change is that the SpireLib interface was using IL types to get layout information to provide through the API, but I had already refactored the layout stuff to be a separate interface, so rewriting the code to just use front-end types was easy enough.

Source/SpireCore/CodeGenerator.cpp
@@ -3,7 +3,6 @@
 #include "CodeWriter.h"
 #include "StringObject.h"
 #include "Naming.h"
-#include "TypeTranslation.h"
 #include "../CoreLib/Tokenizer.h"
 #include <assert.h>
 
@@ -47,6 +46,7 @@ namespace Spire
 			CodeWriter codeWriter;
 			ScopeDictionary<String, ILOperand*> variables;
 			Dictionary<String, RefPtr<ILType>> genericTypeMappings;
+            Dictionary<StructSyntaxNode*, RefPtr<ILStructType>> structTypes;
 
 			void PushStack(ILOperand * op)
 			{
@@ -127,7 +127,7 @@ namespace Spire
 		public:
 			virtual RefPtr<StructSyntaxNode> VisitStruct(StructSyntaxNode * st) override
 			{
-				result.Program->Structs.Add(symTable->Structs[st->Name.Content]()->Type);
+                TranslateStructType(st, &genericTypeMappings);
 				return st;
 			}
 			virtual void ProcessFunction(FunctionSyntaxNode * func) override
@@ -945,7 +945,7 @@ namespace Spire
 					}
 					else if (expr->BaseExpression->Type->IsStruct())
 					{
-						int id = expr->BaseExpression->Type->AsBasicType()->Struct->SyntaxNode->FindField(expr->MemberName);
+						int id = expr->BaseExpression->Type->AsBasicType()->structDecl->FindField(expr->MemberName);
 						GenerateIndexExpression(base, result.Program->ConstantPool->CreateConstant(id),
 							expr->Access == ExpressionAccess::Read);
 					}
@@ -1180,60 +1180,87 @@ namespace Spire
 				result.Program = new ILProgram();
 				codeWriter.SetConstantPool(result.Program->ConstantPool.Ptr());
 			}
+
+        private:
+            RefPtr<ILStructType> TranslateStructType(StructSyntaxNode* structDecl, Dictionary<String, RefPtr<ILType>> * genericTypeMappings = nullptr)
+            {
+                RefPtr<ILStructType> ilStructType;
+
+                if (structTypes.TryGetValue(structDecl, ilStructType))
+                {
+                    return ilStructType;
+                }
+
+                ilStructType = new ILStructType();
+                ilStructType->TypeName = structDecl->Name.Content;
+                ilStructType->IsIntrinsic = structDecl->IsIntrinsic;
+
+
+                for (auto field : structDecl->Fields)
+                {
+                    ILStructType::ILStructField ilField;
+                    ilField.FieldName = field->Name.Content;
+                    ilField.Type = TranslateExpressionType(field->Type.Ptr());
+                    ilStructType->Members.Add(ilField);
+                }
+
+                structTypes.Add(structDecl, ilStructType);
+                return ilStructType;
+            }
+
+		    RefPtr<ILType> TranslateExpressionType(ExpressionType * type, Dictionary<String, RefPtr<ILType>> * genericTypeMappings = nullptr)
+		    {
+			    RefPtr<ILType> resultType = 0;
+			    if (auto basicType = type->AsBasicType())
+			    {
+				    if (basicType->BaseType == BaseType::Struct)
+				    {
+                        resultType = TranslateStructType(basicType->structDecl, genericTypeMappings);
+				    }
+				    else if (basicType->BaseType == BaseType::Record)
+				    {
+					    if (genericTypeMappings)
+						    return (*genericTypeMappings)[basicType->RecordTypeName]();
+					    else
+						    throw InvalidProgramException("unexpected record type.");
+				    }
+				    else if (basicType->BaseType == BaseType::Generic)
+				    {
+					    if (genericTypeMappings)
+						    return (*genericTypeMappings)[basicType->GenericTypeVar]();
+					    else
+						    throw InvalidProgramException("unexpected generic type.");
+				    }
+				    else
+				    {
+					    auto base = new ILBasicType();
+					    base->Type = (ILBaseType)basicType->BaseType;
+					    resultType = base;
+				    }
+			    }
+			    else if (auto arrType = type->AsArrayType())
+			    {
+				    auto nArrType = new ILArrayType();
+				    nArrType->BaseType = TranslateExpressionType(arrType->BaseType.Ptr(), genericTypeMappings);
+				    nArrType->ArrayLength = arrType->ArrayLength;
+				    resultType = nArrType;
+			    }
+			    else if (auto genType = type->AsGenericType())
+			    {
+				    auto gType = new ILGenericType();
+				    gType->GenericTypeName = genType->GenericTypeName;
+				    gType->BaseType = TranslateExpressionType(genType->BaseType.Ptr(), genericTypeMappings);
+				    resultType = gType;
+			    }
+			    return resultType;
+		    }
+
+		    RefPtr<ILType> TranslateExpressionType(const RefPtr<ExpressionType> & type, Dictionary<String, RefPtr<ILType>> * genericTypeMappings = nullptr)
+		    {
+			    return TranslateExpressionType(type.Ptr(), genericTypeMappings);
+		    }
+
 		};
-
-		RefPtr<ILType> TranslateExpressionType(ExpressionType * type, Dictionary<String, RefPtr<ILType>> * genericTypeMappings)
-		{
-			RefPtr<ILType> resultType = 0;
-			if (auto basicType = type->AsBasicType())
-			{
-				if (basicType->BaseType == BaseType::Struct)
-				{
-					resultType = basicType->Struct->Type;
-				}
-				else if (basicType->BaseType == BaseType::Record)
-				{
-					if (genericTypeMappings)
-						return (*genericTypeMappings)[basicType->RecordTypeName]();
-					else
-						throw InvalidProgramException("unexpected record type.");
-				}
-				else if (basicType->BaseType == BaseType::Generic)
-				{
-					if (genericTypeMappings)
-						return (*genericTypeMappings)[basicType->GenericTypeVar]();
-					else
-						throw InvalidProgramException("unexpected generic type.");
-				}
-				else
-				{
-					auto base = new ILBasicType();
-					base->Type = (ILBaseType)basicType->BaseType;
-					resultType = base;
-				}
-			}
-			else if (auto arrType = type->AsArrayType())
-			{
-				auto nArrType = new ILArrayType();
-				nArrType->BaseType = TranslateExpressionType(arrType->BaseType.Ptr(), genericTypeMappings);
-				nArrType->ArrayLength = arrType->ArrayLength;
-				resultType = nArrType;
-			}
-			else if (auto genType = type->AsGenericType())
-			{
-				auto gType = new ILGenericType();
-				gType->GenericTypeName = genType->GenericTypeName;
-				gType->BaseType = TranslateExpressionType(genType->BaseType.Ptr(), genericTypeMappings);
-				resultType = gType;
-			}
-			return resultType;
-		}
-
-		RefPtr<ILType> TranslateExpressionType(const RefPtr<ExpressionType> & type, Dictionary<String, RefPtr<ILType>> * genericTypeMappings)
-		{
-			return TranslateExpressionType(type.Ptr(), genericTypeMappings);
-		}
-
 
 		ICodeGenerator * CreateCodeGenerator(SymbolTable * symbols, CompileResult & result)
 		{

Source/SpireCore/Diagnostics.cpp
@@ -20,9 +20,9 @@ void printDiagnosticArg(StringBuilder& sb, CoreLib::Basic::String const& str)
     sb << str;
 }
 
-void printDiagnosticArg(StringBuilder& sb, StructSymbol* sym)
+void printDiagnosticArg(StringBuilder& sb, Decl* decl)
 {
-    sb << sym->Name;
+    sb << decl->Name.Content;
 }
 
 void printDiagnosticArg(StringBuilder& sb, Type* type)

Source/SpireCore/Diagnostics.h
@@ -68,7 +68,7 @@ namespace Spire
 			}
 		};
 
-        class StructSymbol;
+        class Decl;
         class Type;
         class ExpressionType;
         class ILType;
@@ -76,7 +76,7 @@ namespace Spire
 
         void printDiagnosticArg(StringBuilder& sb, char const* str);
         void printDiagnosticArg(StringBuilder& sb, CoreLib::Basic::String const& str);
-        void printDiagnosticArg(StringBuilder& sb, StructSymbol* sym);
+        void printDiagnosticArg(StringBuilder& sb, Decl* decl);
         void printDiagnosticArg(StringBuilder& sb, Type* type);
         void printDiagnosticArg(StringBuilder& sb, ExpressionType* type);
         void printDiagnosticArg(StringBuilder& sb, ILType* type);

Source/SpireCore/SemanticsVisitor.cpp
@@ -1,6 +1,4 @@
 #include "SyntaxVisitors.h"
-#include "IL.h"
-#include "TypeTranslation.h"
 
 namespace Spire
 {
@@ -158,11 +156,13 @@ namespace Spire
 				else
 				{
 					expType->BaseType = BaseType::Struct;
-					RefPtr<StructSymbol> ssym;
-					if (symbolTable->Structs.TryGetValue(typeNode->TypeName, ssym))
-					{
-						expType->Struct = ssym.Ptr();
-					}
+                    if (auto decl = symbolTable->LookUp(typeNode->TypeName))
+                    {
+                        if (auto structDecl = dynamic_cast<StructSyntaxNode*>(decl))
+                        {
+                            expType->structDecl = structDecl;
+                        }
+                    }
 					else if (currentPipeline || currentShader)
 					{
 						PipelineSymbol * pipe = currentPipeline ? currentPipeline : currentShader->Pipeline;
@@ -778,11 +778,7 @@ namespace Spire
 				this->function = nullptr;
 				for (auto & s : program->Structs)
 				{
-					RefPtr<StructSymbol> ssym = new StructSymbol();
-					ssym->Name = s->Name.Content;
-					ssym->SyntaxNode = s;
-					ssym->Type = new ILStructType();
-					symbolTable->Structs.Add(s->Name.Content, ssym);
+                    symbolTable->globalDecls.Add(s->Name.Content, s.Ptr());
 				}
 				for (auto & s : program->Structs)
 					VisitStruct(s.Ptr());
@@ -856,20 +852,10 @@ namespace Spire
 
 			virtual RefPtr<StructSyntaxNode> VisitStruct(StructSyntaxNode * structNode) override
 			{
-				RefPtr<StructSymbol> st;
-				if (symbolTable->Structs.TryGetValue(structNode->Name.Content, st))
-				{
-					st->Type->TypeName = structNode->Name.Content;
-					st->Type->IsIntrinsic = structNode->IsIntrinsic;
-					for (auto node : structNode->Fields)
-					{
-						node->Type = TranslateTypeNode(node->TypeNode);
-						ILStructType::ILStructField f;
-						f.FieldName = node->Name.Content;
-						f.Type = TranslateExpressionType(node->Type.Ptr());
-						st->Type->Members.Add(f);
-					}
-				}
+                for (auto field : structNode->Fields)
+                {
+                    field->Type = TranslateTypeNode(field->TypeNode);
+                }
 				return structNode;
 			}
 
@@ -1863,14 +1849,14 @@ namespace Spire
 				}
 				else if (baseType->IsStruct())
 				{
-					int id = baseType->AsBasicType()->Struct->SyntaxNode->FindField(expr->MemberName);
+					int id = baseType->AsBasicType()->structDecl->FindField(expr->MemberName);
 					if (id == -1)
 					{
 						expr->Type = ExpressionType::Error;
-						getSink()->diagnose(expr, Diagnostics::noMemberOfNameInType, expr->MemberName, baseType->AsBasicType()->Struct);
+						getSink()->diagnose(expr, Diagnostics::noMemberOfNameInType, expr->MemberName, baseType->AsBasicType()->structDecl);
 					}
 					else
-						expr->Type = baseType->AsBasicType()->Struct->SyntaxNode->Fields[id]->Type;
+						expr->Type = baseType->AsBasicType()->structDecl->Fields[id]->Type;
 					if (auto bt = expr->Type->AsBasicType())
 					{
 						bt->IsLeftValue = baseType->AsBasicType()->IsLeftValue;

Source/SpireCore/SpireCore.vcxproj
@@ -390,7 +390,6 @@
     <ClInclude Include="Syntax.h" />
     <ClInclude Include="SyntaxVisitors.h" />
     <ClInclude Include="TypeLayout.h" />
-    <ClInclude Include="TypeTranslation.h" />
     <ClInclude Include="VariantIR.h" />
   </ItemGroup>
   <ItemGroup>

Source/SpireCore/SpireCore.vcxproj.filters
@@ -60,9 +60,6 @@
     <ClInclude Include="Naming.h">
       <Filter>Front End</Filter>
     </ClInclude>
-    <ClInclude Include="TypeTranslation.h">
-      <Filter>Front End</Filter>
-    </ClInclude>
     <ClInclude Include="CLikeCodeGen.h">
       <Filter>Back End</Filter>
     </ClInclude>
@@ -156,12 +153,9 @@
       <Filter>Front End</Filter>
     </ClCompile>
     <ClCompile Include="Preprocessor.cpp">
-<<<<<<< HEAD
       <Filter>Front End</Filter>
     </ClCompile>
     <ClCompile Include="TypeLayout.cpp">
-=======
->>>>>>> Move `TypeTranslation` into `CodeGenerator`
       <Filter>Front End</Filter>
     </ClCompile>
   </ItemGroup>

Source/SpireCore/SymbolTable.cpp
@@ -500,6 +500,14 @@ namespace Spire
 			}).ToList();
 		}
 
+        Decl* SymbolTable::LookUp(String const& name)
+        {
+            Decl* decl = nullptr;
+            if (globalDecls.TryGetValue(name, decl))
+                return decl;
+            return nullptr;
+        }
+
 		int UniqueIdGenerator::currentGUID = 0;
 		void UniqueIdGenerator::Clear()
 		{

Source/SpireCore/SymbolTable.h
@@ -178,14 +178,6 @@ namespace Spire
 
 		class CompileResult;
 
-		class StructSymbol
-		{
-		public:
-			String Name;
-			RefPtr<StructSyntaxNode> SyntaxNode;
-			RefPtr<ILStructType> Type;
-		};
-
 		class SymbolTable
 		{
 		private:
@@ -195,7 +187,7 @@ namespace Spire
 			EnumerableDictionary<String, RefPtr<FunctionSymbol>> Functions; // indexed by internal name
 			EnumerableDictionary<String, RefPtr<ShaderSymbol>> Shaders;
 			EnumerableDictionary<String, RefPtr<PipelineSymbol>> Pipelines;
-			EnumerableDictionary<String, RefPtr<StructSymbol>> Structs;
+			EnumerableDictionary<String, Decl*> globalDecls;
 			List<ShaderSymbol*> ShaderDependenceOrder;
 			bool SortShaders(); // return true if success, return false if dependency is cyclic
 			void EvalFunctionReferenceClosure();
@@ -206,6 +198,8 @@ namespace Spire
 			bool IsWorldImplicitlyReachable(PipelineSymbol * pipe, EnumerableHashSet<String> & src, String targetWorld, RefPtr<ExpressionType> type);
 			bool IsWorldImplicitlyReachable(PipelineSymbol * pipe, String src, String targetWorld, RefPtr<ExpressionType> type);
 			List<ImportPath> FindImplicitImportOperatorChain(PipelineSymbol * pipe, String worldSrc, String worldDest, RefPtr<ExpressionType> type);
+
+            Decl* LookUp(String const& name);
 		};
 
 		class UniqueIdGenerator

commit 7fa0400ae3864e12ac9b4846a0e977268d92466a

Fix bug introduced in 1eee18256190996eddf8c1613ef3fffae0caa372
That change had removed `StructSymbol` from the front-end, and mistakenly removed the logic in `CodeGenerator::VisitStruct` that actually added an `ILStructType` to the list of struct types to be emitted.

This change fixes the issue, and also adds a small test case that actually uses struct types in a few ways.
This test currently doesn't help a whole lot, though, because we don't have any end-to-end testing of generating code (in this case, just checking that the generated GLSL is okay would help).
Doing a better job with our tests should be a distinct change, though.

With this fix, the `SpireMiniEngine` project now seems to run correctly again.

Source/SpireCore/CodeGenerator.cpp
@@ -129,7 +129,8 @@ namespace Spire
 		public:
 			virtual RefPtr<StructSyntaxNode> VisitStruct(StructSyntaxNode * st) override
 			{
-                TranslateStructType(st, &genericTypeMappings);
+                RefPtr<ILStructType> structType = TranslateStructType(st, &genericTypeMappings);
+                result.Program->Structs.Add(structType);
 				return st;
 			}
 			virtual void ProcessFunction(FunctionSyntaxNode * func) override

Tests/FrontEnd/pipeline-simple.spireh
@@ -1,0 +1,84 @@
+// pipeline-simple.spireh
+
+
+// TODO(tfoley): strip this down to a minimal pipeline
+
+pipeline StandardPipeline
+{
+    [Pinned]
+    input world MeshVertex;
+    
+    [Pinned]
+    input world ModelInstance;
+    
+    [Pinned]
+    
+    input world ViewUniform;
+    
+    [Pinned]
+    input world MaterialUniform;
+    
+    world CoarseVertex;// : "glsl(vertex:projCoord)" using projCoord export standardExport;
+    world Fragment;// : "glsl" export fragmentExport;
+    
+    require @CoarseVertex vec4 projCoord; 
+    
+    [Binding: "2"]
+    extern @(CoarseVertex*, Fragment*) Uniform<MaterialUniform> MaterialUniformBlock; 
+    import(MaterialUniform->CoarseVertex) uniformImport()
+    {
+        return project(MaterialUniformBlock);
+    }
+    import(MaterialUniform->Fragment) uniformImport()
+    {
+        return project(MaterialUniformBlock);
+    }
+
+    [Binding: "1"]
+    extern @(CoarseVertex*, Fragment*) Uniform<ViewUniform> ViewUniformBlock;
+    import(ViewUniform->CoarseVertex) uniformImport()
+    {
+        return project(ViewUniformBlock);
+    }
+    import(ViewUniform->Fragment) uniformImport()
+    {
+        return project(ViewUniformBlock);
+    }
+    
+    [Binding: "0"]
+    extern @(CoarseVertex*, Fragment*) Uniform<ModelInstance> ModelInstanceBlock;
+    import(ModelInstance->CoarseVertex) uniformImport()
+    {
+        return project(ModelInstanceBlock);
+    }
+    import(ModelInstance->Fragment) uniformImport()
+    {
+        return project(ModelInstanceBlock);
+    }
+    
+    [VertexInput]
+    extern @CoarseVertex MeshVertex vertAttribIn;
+    import(MeshVertex->CoarseVertex) vertexImport()
+    {
+        return project(vertAttribIn);
+    }
+    
+    extern @Fragment CoarseVertex CoarseVertexIn;
+    import(CoarseVertex->Fragment) standardImport()
+// TODO(tfoley): this trait doesn't seem to be implemented on `vec3`
+//        require trait IsTriviallyPassable(CoarseVertex)
+    {
+        return project(CoarseVertexIn);
+    }
+    
+    stage vs : VertexShader
+    {
+        World: CoarseVertex;
+        Position: projCoord;
+    }
+    
+    stage fs : FragmentShader
+    {
+        World: Fragment;
+    }
+}

Tests/FrontEnd/struct.spire
@@ -1,0 +1,42 @@
+// test that `struct` decls work
+
+#include "pipeline-simple.spireh"
+
+// struct declaration
+struct Foo
+{
+	vec3 a;
+	vec3 b;
+};
+
+// function on a struct
+Foo makeFoo(float x, float y)
+{
+	// local of struct type
+	Foo foo;
+	foo.a = vec3(x);
+	foo.b = vec3(y);
+	return foo;
+}
+
+shader Test : StandardPipeline
+{
+	// Uniform of struct type
+	@MaterialUniform Foo foo1;
+
+    @MeshVertex vec3 position;
+    @MeshVertex vec3 color;
+
+    @ModelInstance mat4 modelViewProjection;
+
+    public vec4 projCoord = modelViewProjection * vec4(position, 1.0);
+
+    // Component of struct type
+    // Note(tfoley): use of `public` here required to work around parser limitations
+    public Foo foo2 = makeFoo(color.x, color.y);
+
+    //
+    vec3 result = foo1.a + foo2.b;
+
+    out @Fragment vec4 colorTarget = vec4(result,1);
+}