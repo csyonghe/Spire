8ceea7c0f86655ec139dc30d4ca831659c57db58
Tim Foley <tfoley@nvidia.com>
2017-02-08 12:57:55 -0800

    Add HLSL test shaders from DX SDK.
    I'm not sure I got everything, but this is supposed to be all the `.hlsl` and `.fx` files from the DirectX SDK (back when that was a thing).
    
    I didn't add annotions to these files to let the test runner compile them, so this checking is hopefully clean copies of the original code.

--- Tests/HLSL/DXSDK/MultithreadedRendering11/MultithreadedRendering11_VS.hlsl
+++ Tests/HLSL/DXSDK/MultithreadedRendering11/MultithreadedRendering11_VS.hlsl
@@ -1,19 +1,24 @@
-//SPIRE_TEST_OPTS:-entry VSMain -profile vs_4_0
 //--------------------------------------------------------------------------------------
-// File: BasicHLSL11_VS.hlsl
+// File: MultithreadedRendering11_VS.hlsl
 //
-// The vertex shader file for the BasicHLSL11 sample.  
+// The vertex shader file for the MultithreadedRendering11 sample.  
 // 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 //--------------------------------------------------------------------------------------
+
+// Various debug options
+//#define UNCOMPRESSED_VERTEX_DATA  // The sdkmesh file contained uncompressed vertex data
 
 //--------------------------------------------------------------------------------------
 // Globals
 //--------------------------------------------------------------------------------------
 cbuffer cbPerObject : register( b0 )
 {
-	matrix		g_mWorldViewProjection	: packoffset( c0 );
-	matrix		g_mWorld				: packoffset( c4 );
+	matrix		g_mWorld	: packoffset( c0 );
+};
+cbuffer cbPerScene : register( b1 )
+{
+	matrix		g_mViewProj	: packoffset( c0 );
 };
 
 //--------------------------------------------------------------------------------------
@@ -24,14 +29,26 @@ struct VS_INPUT
 	float4 vPosition	: POSITION;
 	float3 vNormal		: NORMAL;
 	float2 vTexcoord	: TEXCOORD0;
+	float3 vTangent		: TANGENT;
 };
 
 struct VS_OUTPUT
 {
 	float3 vNormal		: NORMAL;
+	float3 vTangent		: TANGENT;
 	float2 vTexcoord	: TEXCOORD0;
+	float4 vPosWorld	: TEXCOORD1;
 	float4 vPosition	: SV_POSITION;
 };
+
+// We aliased signed vectors as a unsigned format. 
+// Need to recover signed values.  The values 1.0 and 2.0
+// are slightly inaccurate here.
+float3 R10G10B10A2_UNORM_TO_R32G32B32_FLOAT( in float3 vVec )
+{
+    vVec *= 2.0f;
+    return vVec >= 1.0f ? ( vVec - 2.0f ) : vVec;
+}
 
 //--------------------------------------------------------------------------------------
 // Vertex Shader
@@ -40,8 +57,16 @@ VS_OUTPUT VSMain( VS_INPUT Input )
 {
 	VS_OUTPUT Output;
 	
-	Output.vPosition = mul( Input.vPosition, g_mWorldViewProjection );
-	Output.vNormal = mul( Input.vNormal, (float3x3)g_mWorld );
+#ifndef UNCOMPRESSED_VERTEX_DATA
+	// Expand compressed vectors
+	Input.vNormal = R10G10B10A2_UNORM_TO_R32G32B32_FLOAT( Input.vNormal );
+	Input.vTangent = R10G10B10A2_UNORM_TO_R32G32B32_FLOAT( Input.vTangent );
+#endif  // #ifndef UNCOMPRESSED_VERTEX_DATA
+	
+	Output.vPosWorld = mul( Input.vPosition,    g_mWorld );
+	Output.vPosition = mul( Output.vPosWorld,   g_mViewProj );
+	Output.vNormal   = mul( Input.vNormal,      (float3x3)g_mWorld );
+	Output.vTangent  = mul( Input.vTangent,     (float3x3)g_mWorld );
 	Output.vTexcoord = Input.vTexcoord;
 	
 	return Output;
