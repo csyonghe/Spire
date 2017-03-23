40f08e700f8ac2a6e3cedd2ea66bdccf20f8a8b5
Yong He <yonghe@outlook.com>
2017-03-23 16:55:08 -0400

    bug fix

--- Tests/HLSLCodeGen/DeferredLighting.fs.hlsl
+++ Tests/HLSLCodeGen/DeferredLighting.fs.hlsl
@@ -6,26 +6,11 @@ float3 pos;
 float3 tangent;
 float3 binormal;
 };
-Texture2D DeferredLightingParams_albedoTex: register(t0, space-1);
-Texture2D DeferredLightingParams_pbrTex: register(t1, space-1);
-Texture2D DeferredLightingParams_normalTex: register(t2, space-1);
-Texture2D DeferredLightingParams_depthTex: register(t3, space-1);
-SamplerState DeferredLightingParams_nearestSampler: register(s0, space-1);
-SamplerState ForwardBasePassParams_textureSampler: register(s1, space-1);
-Texture2DArray lighting_shadowMapArray: register(t4, space-1);
-SamplerComparisonState lighting_shadowMapSampler: register(s2, space-1);
-TextureCube lighting_envMap: register(t5, space-1);
-Texture2D layers_l0_albedoTex: register(t6, space-1);
-SamplerState layers_l0_samplerState: register(s3, space-1);
-Texture2D layers_l1_albedoTex: register(t7, space-1);
-SamplerState layers_l1_samplerState: register(s4, space-1);
-Texture2D layers_l2_albedoTex: register(t8, space-1);
-SamplerState layers_l2_samplerState: register(s5, space-1);
-Texture2D DeferredLightingParams_albedoTex: register(t0, space0);
-Texture2D DeferredLightingParams_pbrTex: register(t1, space0);
-Texture2D DeferredLightingParams_normalTex: register(t2, space0);
-Texture2D DeferredLightingParams_depthTex: register(t3, space0);
-SamplerState DeferredLightingParams_nearestSampler: register(s0, space0);
+Texture2D DeferredLightingParams_albedoTex: register(t0);
+Texture2D DeferredLightingParams_pbrTex: register(t1);
+Texture2D DeferredLightingParams_normalTex: register(t2);
+Texture2D DeferredLightingParams_depthTex: register(t3);
+SamplerState DeferredLightingParams_nearestSampler: register(s0);
 cbuffer bufForwardBasePassParams : register(b1)
 {
 struct {
@@ -37,7 +22,7 @@ float3 cameraPos;
 float time;
 } ForwardBasePassParams;
 };
-SamplerState ForwardBasePassParams_textureSampler: register(s0, space1);
+SamplerState ForwardBasePassParams_textureSampler: register(s1);
 cbuffer buflighting : register(b2)
 {
 struct {
@@ -50,15 +35,15 @@ float4x4 lightMatrix[8];
 float4 zPlanes[2];
 } lighting;
 };
-Texture2DArray lighting_shadowMapArray: register(t0, space2);
-SamplerComparisonState lighting_shadowMapSampler: register(s0, space2);
-TextureCube lighting_envMap: register(t1, space2);
-Texture2D layers_l0_albedoTex: register(t0, space3);
-SamplerState layers_l0_samplerState: register(s0, space3);
-Texture2D layers_l1_albedoTex: register(t1, space3);
-SamplerState layers_l1_samplerState: register(s1, space3);
-Texture2D layers_l2_albedoTex: register(t2, space3);
-SamplerState layers_l2_samplerState: register(s2, space3);
+Texture2DArray lighting_shadowMapArray: register(t4);
+SamplerComparisonState lighting_shadowMapSampler: register(s2);
+TextureCube lighting_envMap: register(t5);
+Texture2D layers_l0_albedoTex: register(t6);
+SamplerState layers_l0_samplerState: register(s3);
+Texture2D layers_l1_albedoTex: register(t7);
+SamplerState layers_l1_samplerState: register(s4);
+Texture2D layers_l2_albedoTex: register(t8);
+SamplerState layers_l2_samplerState: register(s5);
 float PhongApprox(float p_Roughness, float p_RoL);
 float3 EnvBRDFApprox(float3 p_SpecularColor, float p_Roughness, float p_NoV);
 float DeferredLighting_selfShadow_vec3(float3 p0_x);
